0028a9122ff6847c1f040811e14f13dc4bff14a8
Nguyễn Hiếu <nguyenhieust85@gmail.com>
2026-09-25 23:56:27 +0700

    Revert "feat: add full database schema and migrations"
    This reverts commit 1a8835a5275461f5b1abfd29ef61c630b687835f.

--- HeThongDatBan-GoiMon/Program.cs
+++ HeThongDatBan-GoiMon/Program.cs
@@ -1,6 +1,4 @@
 using HeThongDatBan_GoiMon.Services;
-using HeThongDatBan_GoiMon.Data;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<IMenuItemService, MenuItemService>();
 builder.Services.AddSingleton<IOrderService, OrderService>();
-builder.Services.AddDbContext<RestaurantDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
